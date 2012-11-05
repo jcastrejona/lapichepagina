9a8637a1a87c203026b5f05d2d7bf1a859c4a96c
daniel torres <daniel.torrer@gmail.com>
2012-11-05 17:36:22 -0600

    meh

--- application/views/home.php
+++ application/views/home.php
@@ -30,7 +30,7 @@
 					<li class="menu"><a id="m1" href="#convocatoria">Convocatoria</a></li>
 					<li class="menu"><a id="m2" href="#equipo">Equipo</a></li>
 					<li class="menu"><a id="m3" href="#proyectos">Proyectos</a></li>
-					<li class="menu"><a id="m4" href="<?= base_url() ?>welcome/index">El matadero</a></li>
+					<li class="menu"><a id="m4" href="<?= base_url() ?>welcome/matadero">El matadero</a></li>
 					<li class="menu"><a id="m5" href="#">Medios</a></li>
 				</ul>
 			</div>
