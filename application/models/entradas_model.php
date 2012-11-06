a483709f0d32fa24713507d373da93b79762d794
Cristóbal Israel Torres Melgoza <delebrindel@gmail.com>
2012-11-06 14:41:30 -0600

    Ultima Modificación
    Versión 1.7.yaperdilacuenta

--- application/models/entradas_model.php
+++ application/models/entradas_model.php
@@ -4,4 +4,36 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/**
+ * Description of info_model
+ *
+ * @author America Movil
+ */
+class entradas_model extends CI_Model {
+
+    function geinfo() {
+        $query = $this->db->get('C2_Entradas');
+        return $query->result_array();
+    }
+    function gsinglee($id_entra){
+        $this->db->where('ID',$id_entra);
+        $query = $this->db->get('C2_Entradas');
+        $row = $query->row();
+        $data = array(
+            'eid' => $row->ID,
+            'etitle' => $row->Titulo,
+            'eres' => $row->Resumen,
+            'ecue' => $row->Cuerpo,
+            'efec' => $row->Fecha,
+            'eimg' => $row->Ruta_imagen,
+            'eaut'=>$row->Autor
+        );
+        return $data;
+        
+    }
+
+}
+
 ?>
+
